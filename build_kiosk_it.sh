68d94601a28472e03648ec4e7d2a16411e884128
fdaluisio <fdaluisio@fedoraproject.org>
2011-11-20 18:15:32 +0100

    Add gitignore, change build_kiosk_it.sh

--- build_kiosk_it.sh
+++ build_kiosk_it.sh
@@ -11,16 +11,29 @@ TMPDIR="${RUNDIR}/tmp/"
 CACHEDIR="${RUNDIR}/cache/"
 KSFILE="${RUNDIR}/kiosk-it.ks"
 
+
+echo '##########################################################'
+echo '#             Fedora Italian Kiosk Spin                  #'
+echo '##########################################################'
+
+
 if [ ! -e /usr/share/spin-kickstarts/fedora-live-base.ks ]; then
 	echo "Please install fedora-kickstarts package"
 	echo "# yum -y install fedora-kickstarts"
 	exit -1
 fi
 
+if [ ! -d ${TMPDIR} ]; then
+	mkdir ${TMPDIR}
+fi
+
+if [ ! -d ${CACHEDIR} ]; then
+        mkdir ${CACHEDIR}
+fi
+
 if [ -x /usr/bin/livecd-creator ]; then
-	#nohup "sudo setarch=i686 /usr/bin/livecd-creator -t ${TMPDIR} -f ${KIOSKNAME} -c ${KSFILE} --cache=${CACHEDIR}" > ${KIOSKNAME}-creation.log &
 	echo "Require root password"
-	su -c "nohup setarch=i686 /usr/bin/livecd-creator -t ${TMPDIR} -f ${KIOSKNAME} -c ${KSFILE} --cache=${CACHEDIR} > ${KIOSKNAME}-creation.log"
+	su -c "nohup setarch i686 /usr/bin/livecd-creator -t ${TMPDIR} -f ${KIOSKNAME} -c ${KSFILE} --cache=${CACHEDIR} > ${KIOSKNAME}-`date +%d%M%Y`.log"
 else
 	echo "please install fedora livecd-tools packages"
 fi
